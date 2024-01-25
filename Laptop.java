a8065aa7177f385fdd91f09018f2fcff49e23ce9
«StasyStar» <Anstar1991@gmail.com>
2024-01-26 00:00:42 +0300

    hw_senimar6

---
+++ Laptop.java
@@ -0,0 +1,76 @@
+package mine2;
+
+import java.util.Objects;
+
+public class Laptop {
+    private int id;
+    private String model;
+    private String color;
+    private int ram;
+    private int ssd;
+    private String os;
+
+
+    public Laptop (int id, String model, String color, int ram, int ssd, String os) {
+        this.id = id;
+        this.model = model;
+        this.color = color;
+        this.ram = ram;
+        this.ssd = ssd;
+        this.os = os;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public String getModel() {
+        return model;
+    }
+    public String getColor() {
+        return color;
+    }
+    public int getRam() {
+        return ram;
+    }
+    public int getSsd() {
+        return ssd;
+    }
+    public String getOs() {
+        return os;
+    }
+
+    public String setColor() {
+        return color;
+    }
+    public int setRam() {
+        return ram;
+    }
+    public int setSsd() {
+        return ssd;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("ID: %d, Модель: %s, Цвет: %s, Операционная память: %dMb, Объем накопителей SSD: %dMb, Операционная система: %s", id, model, color, ram, ssd, os);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, model, color, ram, ssd, os);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        Laptop l = (Laptop) obj;
+        return id == l.id && model.equals(l.model) && color.equals(l.color) && ram == l.ram && ssd == l.ssd && os.equals(l.os);
+    }
+}
+
+  
---
+++ Program.java
@@ -0,0 +1,131 @@
+package mine2;
+
+// Задание
+
+// -Подумать над структурой класса Ноутбук для магазина техники - выделить поля и методы. Реализовать в java.
+// - Создать множество ноутбуков.
+// - Написать метод, который будет запрашивать у пользователя критерий (или критерии) фильтрации и выведет ноутбуки, отвечающие фильтру. Критерии фильтрации можно хранить в Map. Например: “Введите цифру, соответствующую необходимому критерию:
+// 1 - ОЗУ
+// 2 - Объем ЖД
+// 3 - Операционная система
+// 4 - Цвет …
+// - Далее нужно запросить минимальные значения для указанных критериев - сохранить параметры фильтрации можно также в Map.
+// - Отфильтровать ноутбуки их первоначального множества и вывести проходящие по условиям.
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+
+
+public class Program {
+
+    public static Set<Laptop> creatingObjectsLaptops() {
+        Laptop laptop1 = new Laptop(1, "MacBook", "grey", 8, 512, "MacOS");
+        Laptop laptop2 = new Laptop(2, "MacBook", "white", 8, 1024, "MacOS");
+        Laptop laptop3 = new Laptop(2, "MacBook", "white", 8, 1024, "MacOS");
+        Laptop laptop4 = new Laptop(3, "Lenovo", "black", 16, 256, "Windows");
+        Laptop laptop5 = new Laptop(4, "Lenovo", "black", 16, 512, "Windows");
+        Laptop laptop6 = new Laptop(5, "HP", "grey", 8, 256, "Windows");
+        Laptop laptop7 = new Laptop(6, "Aser", "silver", 16, 1024, "Linux");
+        Laptop laptop8 = new Laptop(7, "Asus", "grey", 16, 512, "Windows");
+        
+        Set<Laptop> laptopsArr = new HashSet<>(Arrays.asList(laptop1, laptop2, laptop3, laptop4, laptop5, laptop6, laptop7, laptop8));
+        return laptopsArr;
+    }
+
+    public static void printObjectsLaptops(Set<Laptop> laptopsArr) {
+        for (Laptop laptop : laptopsArr) {
+            System.out.println(laptop);
+        }
+    }
+
+    public static HashMap<String, String> userData() {
+        HashMap<Integer, String> characteristics = new HashMap<>();
+        characteristics.put(0, "stop");    
+        characteristics.put(1, "model");
+        characteristics.put(2, "color");
+        characteristics.put(3, "ram");
+        characteristics.put(4, "ssd");
+        characteristics.put(5, "os");
+
+        Scanner sc = new Scanner(System.in);
+        boolean flag = true;
+        HashMap<String, String> filterParameter = new HashMap<>();
+
+        while (flag) {
+            for (int i : characteristics.keySet()) {
+                System.out.println(i + ". " + characteristics.get(i));     
+            }
+            System.out.println("Enter a number of filter parametr or choose 0 to stop: ");
+
+            int parameter = sc.nextInt();
+            sc.nextLine();
+
+            switch (parameter) {
+                case 0:
+                    flag = false;
+                    break;
+                case 1:
+                    System.out.println("Enter a model (MacBook, Lenovo, HP, Aser, Asus): ");
+                    String valueParam = sc.nextLine();
+                    filterParameter.put("model", valueParam);
+                    break;
+                case 2:
+                    System.out.println("Enter a color (grey, white, black, silver): ");
+                    String valueParam2 = sc.nextLine();
+                    filterParameter.put("color", valueParam2);
+                    break;
+                case 3:
+                    System.out.println("Enter a minimal value for the parametr (8Gb, 16Gb): ");
+                    String valueParam3 = sc.nextLine();
+                    filterParameter.put("ram", valueParam3);
+                    break;
+                case 4:
+                    System.out.println("Enter a minimal value for the parametr (256Gb, 512Gb, 1024Gb): ");
+                    String valueParam4 = sc.nextLine();
+                    filterParameter.put("ssd", valueParam4);
+                    break;
+                case 5:
+                    System.out.println("Enter a minimal an os: (MacOS, Windows, Linux)");
+                    String valueParam5 = sc.nextLine();
+                    filterParameter.put("os", valueParam5);
+                    break;
+            }
+        }
+        return filterParameter;
+    }
+
+    public static Set<Laptop> filterResult(Set<Laptop> laptopsArr){
+        Map<String, String> filterParameter = userData();
+        Set<Laptop> filterLaptop = new HashSet<>();
+        for (Laptop laptop: laptopsArr){
+            if(laptop.getModel().equals(filterParameter.get("model")) || 
+            filterParameter.get("model") == null || 
+            laptop.getColor().equals(filterParameter.get("color")) || 
+            filterParameter.get("color") == null || 
+            laptop.getRam() >= Integer.parseInt(filterParameter.get("ram")) || 
+            filterParameter.get("ram") == null || 
+            laptop.getSsd() >= Integer.parseInt(filterParameter.get("ssd")) || 
+            filterParameter.get("ssd") == null || 
+            laptop.getOs().equals(filterParameter.get("os")) || 
+            filterParameter.get("os") == null){
+                filterLaptop.add(laptop);
+            } else {
+                System.out.println("There is no laptops with such parametrs!");
+            }
+        }
+        return filterLaptop;
+    }
+
+    public static void main(String[] args) {
+        Set<Laptop> laptopsArr = creatingObjectsLaptops();
+        // printObjectsLapt1ops(laptopsArr); // распечатка всех имеющихся компьютеров
+        Set<Laptop> filterLaptops = filterResult(laptopsArr);
+        printObjectsLaptops(filterLaptops);
+        System.out.println(userData());
+    }
+    
+}
